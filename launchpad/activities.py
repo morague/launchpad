9cfb6d528bfb89c4c022b767481358f2633753e4
rviry <rom88.viry@gmail.com>
2024-06-24 17:45:49 +0200

    (launchpad.watcher) Groups & watcher impl

--- launchpad/activities.py
+++ launchpad/activities.py
@@ -9,12 +9,14 @@ from datetime import datetime
 from hashlib import sha256
 from collections import deque
 from pathlib import Path
+from itertools import chain
+from collections import ChainMap
 
 import importlib, importlib.util
 from importlib.machinery import SourceFileLoader
 
 from types import ModuleType
-from typing import Callable, Type
+from typing import Callable, Type, Optional
 
 from launchpad.parsers import parse_yaml
 from launchpad.utils import is_activity, is_workflow, path_into_module
@@ -44,169 +46,3 @@ for module_path in modules_paths:
     activities.update(load_activities(module_path))
     
 
-
-class Module:
-    __module: Path
-    __latest: str
-    __historics: list[tuple[Datetime, str]]
-    changes: bool = False
-    
-    @property
-    def latest(self):
-        return self.__latest
-    
-    @property
-    def historics(self):
-        return self.__historics
-    
-    @property
-    def module(self):
-        return self.__module
-    
-    @property
-    def ftype(self):
-        return self.module.suffix.strip(".")
-    
-    def __init__(self, module_fp: str) -> None:
-        self.__module = Path(module_fp)
-        self.__historics = []
-        self._update_latest(self.version())
-    
-    def __repr__(self) -> str:
-        return f"<{self.module.as_posix()}:latest {self.latest} | changes: {self.changes}>"
-        
-    def version(self):
-        with open(self.module.absolute(), 'r') as f:
-            version = sha256(f.read().encode()).hexdigest()
-        return version
-            
-    def watch(self):
-        version = self.version()
-        if version != self.latest:
-            self.changes = True
-            self._update_latest(version)
-    
-    def changes_resolved(self):
-        self.changes = False
-
-    def same(self, other: Path) -> bool:
-        return self.module.samefile(other)
-
-    def _update_latest(self, version: str) -> None:
-        self.__latest = version
-        self.__historics.insert(0, ((datetime.now().isoformat(), version)))
-
-
-class YamlModule(Module):    
-    def __init__(self, module_fp: str) -> None:
-        super().__init__(module_fp)
-    
-    def payload(self):
-        return parse_yaml(self.module.absolute())
-    
-class PyModule(Module):    
-    def __init__(self, module_fp: str) -> None:
-        super().__init__(module_fp)
-        
-    @property
-    def module_name(self):
-        file = self.module.stem
-        path = str(self.module.relative_to("./")).split("/")[:-1]
-        return ".".join(path + [file])
-    
-    def load(self) -> ModuleType:
-        spec = importlib.util.spec_from_file_location(
-            self.module_name, 
-            self.module.absolute()
-        )
-        module = importlib.util.module_from_spec(spec)
-        sys.modules[self.module_name] = module
-        spec.loader.exec_module(module)
-        return module
-
-class Watcher(object):
-    __roots: list[str] = ["./"]
-    __modules: dict[str, Module]
-    _skip: list[str] = ["asgi.py"]
-
-    @property
-    def roots(self):
-        return self.__roots
-    
-    @property
-    def modules(self):
-        return self.__modules
-    
-    def __init__(self, *paths) -> None:
-        self.__modules = {}
-        for fp in paths:
-            path = Path(fp)
-            if path.is_dir():
-                self.__roots.append(fp)
-            else:
-                self.__modules[fp] = Module(fp)
-        self.__roots = list(set(self.__roots))
-
-    @classmethod
-    def initialize(cls, *paths) -> Watcher:
-        watcher = cls(*paths)
-        watcher.visit()
-        return watcher
-
-    def visit(self) -> None:
-        files = []
-        [files.extend(self._explore_folder(p)) for p in self.roots]
-        for file in files:
-            self._register_file(file)
-        self._remove_stale_files(files)
-        
-    def refresh(self) -> None:
-        for fp, mod in self.modules.items():
-            if mod.changes:
-                name = path_into_module(fp)
-                importlib.reload(name)
-                mod.changes_resolved()
-
-    def load_temporal_objects(self) -> dict[str, Callable]:
-        objects = {}
-        for name in self.pymodules().keys():
-            name = path_into_module(name)
-            module = importlib.import_module(name=name)
-            for k,v in module.__dict__.items():
-                if k.startswith("__"):
-                    continue
-                
-                # ref = objects.get(k, None)
-                # if ref is not None and (is_activity(v) or is_workflow(v)) and ref != v:
-                #     raise KeyError()
-                
-                if(is_activity(v) or is_workflow(v)):
-                    objects[k] = v
-        # sys.modules[inspect.stack()[1].filename].__dict__.update(objects)
-        return objects
-
-    def pymodules(self) -> dict[str, Module]:
-        return {k:v for k,v in self.modules.items() if k.endswith(".py")}
-    
-    def _register_file(self, fp: str) -> None:
-        ref = self.modules.get(fp, None)
-        if Path(fp).name in self._skip:
-            return
-        elif ref is None:
-            self.__modules[fp] = Module(fp)
-        else:
-            self.modules[fp].watch()
-            
-    def _remove_stale_files(self, visited_files: list[str]) -> None:
-        removed = list(set(self.modules.keys()) - set(visited_files))
-        [self.__modules.pop(fp) for fp in removed]
-    
-    def _explore_folder(self, path) -> list[str]:
-        py = glob.glob(f"{path}/**/[!_]*.py", recursive=True)
-        yaml = glob.glob(f"{path}/**/[!_]*.yaml", recursive=True)
-        yml = glob.glob(f"{path}/**/[!_]*.yml", recursive=True)
-        return py + yaml + yml
-    
-        
-    
-    
---
+++ launchpad/watcher.py
@@ -0,0 +1,301 @@
+from __future__ import annotations
+
+import sys
+import glob
+
+from datetime import datetime
+from hashlib import sha256
+from pathlib import Path
+from collections import ChainMap, deque
+
+import importlib, importlib.util
+
+from types import ModuleType
+from typing import Callable, Type, Optional
+
+from launchpad.parsers import parse_yaml
+from launchpad.utils import is_activity, is_workflow
+
+Datetime = str
+
+class Module:
+    __module: Path
+    __latest: str
+    __historics: list[tuple[Datetime, str]]
+    changes: bool = False
+    
+    @property
+    def latest(self):
+        return self.__latest
+    
+    @property
+    def historics(self):
+        return self.__historics
+    
+    @property
+    def module(self):
+        return self.__module
+    
+    @property
+    def ftype(self):
+        return self.module.suffix.strip(".")
+    
+    def __init__(self, module_fp: Path, new: bool= False) -> None:
+        self.__module = module_fp
+        self.__historics = []
+        self._update_latest(self.version())
+        if new:
+            self.changes = True
+    
+    def __repr__(self) -> str:
+        return f"<{self.module}:latest {self.latest} | changes: {self.changes}>"
+
+    def version(self):
+        with open(self.module.absolute(), 'r') as f:
+            version = sha256(f.read().encode()).hexdigest()
+        return version
+            
+    def watch(self):
+        version = self.version()
+        if version != self.latest:
+            self.changes = True
+            self._update_latest(version)
+    
+    def changes_resolved(self):
+        self.changes = False
+
+    def same(self, other: Path) -> bool:
+        return self.module.samefile(other)
+
+    def _update_latest(self, version: str) -> None:
+        self.__historics.insert(0, ((datetime.now().isoformat(), version)))
+        self.__latest = version
+
+
+class YamlModule(Module):    
+    def __init__(self, module_fp: str, new: bool= False) -> None:
+        super().__init__(module_fp, new)
+    
+    def payload(self):
+        return parse_yaml(self.module.absolute())
+    
+class PyModule(Module):    
+    def __init__(self, module_fp: str, new: bool= False) -> None:
+        super().__init__(module_fp, new)
+        
+    @property
+    def module_name(self):
+        file = self.module.stem
+        path = str(self.module.relative_to("./")).split("/")[:-1]
+        return ".".join(path + [file])
+    
+    def reload(self) -> ModuleType:
+        spec = importlib.util.spec_from_file_location(
+            self.module_name, 
+            self.module.absolute()
+        )
+        module = importlib.util.module_from_spec(spec)
+        sys.modules[self.module_name] = module
+        spec.loader.exec_module(module)
+        return module
+    
+    def temporal_objects(self) -> dict[str, Callable]:
+        objects = {}
+        for k,v in sys.modules[self.module_name].__dict__.items():
+            if k.startswith("__"):
+                    continue
+            if(is_activity(v) or is_workflow(v)):
+                objects[k] = v
+        return objects
+    
+    def inject(self, objects: dict[str, Callable]) -> None:
+        sys.modules[self.module_name].__dict__.update(objects)
+
+class Group(object):
+    __name: str
+    __basepaths: list[Path]    
+    __modules: dict[Path, Module]
+    skip: list[Path]
+    
+    @property
+    def name(self):
+        return self.__name
+    
+    @property
+    def modules(self):
+        return self.__modules    
+
+    @property
+    def basepaths(self):
+        return self.__basepaths
+    
+    @property
+    def paths(self):
+        paths = []
+        for p in self.basepaths:
+            if p in self.skip:
+                continue            
+            if p.is_dir():
+                paths.extend(self._explore(p))
+            elif p.is_file():
+                paths.append(p)
+        return paths
+    
+    def __init__(self, name: str, paths: list[Path], skip: list[Path] = []) -> None:
+        self.__name = name
+        self.__basepaths = paths
+        self.__modules = {}
+        self.skip = skip
+        
+    @classmethod
+    def initialize(cls, name: str, paths: list[Path], skip: list[Path] = []) -> Group:
+        group = cls(name, paths, skip)
+        group.visit()
+        return group
+    
+    def visit(self):
+        new = True
+        if len(self.modules.keys()) == 0:
+            new = False
+        self._remove_stale_module()
+        self._watch_modules()
+        self._register_module(new)
+    
+    def reload(self) -> None:
+        for _, module in self.pymodules().items():
+            if module.changes:
+                module.reload()
+                
+    def temporal_objects(self) -> dict[str, Callable]:
+        objects = {}
+        for _, module in self.pymodules().items():
+            objects.update(module.temporal_objects())
+        return objects
+            
+    def payloads(self) -> dict[str, dict]:
+        payloads = {}
+        for name, yaml in self.yamlmodules().items():
+            payloads.update({name: yaml.payload()})
+        return payloads
+            
+    def pymodules(self) -> dict[str, PyModule]:
+        return {k:v for k,v in self.modules.items() if k.endswith(".py")}
+
+    def yamlmodules(self) -> dict[str, YamlModule]:
+        return {k:v for k,v in self.modules.items() if (k.endswith(".yaml") or k.endswith(".yml"))}
+
+    def _watch_modules(self):
+        [module.watch() for module in self.modules.values()]
+
+    def _register_module(self, new: bool= False):
+        new_paths = list(set(self.paths) - set(self.modules.keys()))
+        for path in new_paths:
+            if path.suffix in [".yml", ".yaml"]:
+                self.__modules[path] = YamlModule(path, new)
+            elif path.suffix == ".py":
+                self.__modules[path] = PyModule(path, new)
+            
+    def _remove_stale_module(self):
+        removed_paths = list(set(self.modules.keys()) - set(self.paths))
+        [self.__modules.pop(p) for p in removed_paths]
+
+    def _explore(self, path: Path) -> list[Path]:
+        abspath = path.absolute()
+        py = glob.glob(f"{abspath}/**/[!_]*.py", recursive=True)
+        yaml = glob.glob(f"{abspath}/**/[!_]*.yaml", recursive=True)
+        yml = glob.glob(f"{abspath}/**/[!_]*.yml", recursive=True)
+        return [Path(p) for p in py + yaml + yml]
+        
+    
+
+class Watcher(object):
+    __basepaths: list[Path]
+    __groups: dict[str, Group]
+    skip: list[Path]
+
+    @property
+    def basepaths(self):
+        return self.__basepaths
+    
+    @property
+    def paths(self):
+        paths = []
+        [paths.extend(g.paths) for g in self.groups]
+        return paths
+
+    @property
+    def groups(self):
+        return self.__groups
+    
+    def __init__(self, *paths: str | Path, skip: list[str | Path] | None = None, **groups: list[str | Path]) -> None:
+        self.__groups = {}
+        self.__basepaths = []
+        self.skip = []
+        if skip  is not None:
+            self.skip = [Path(p) if isinstance(p, str) else p for p in skip]
+        
+        self.add_group("others", [Path(p) if isinstance(p, str) else p for p in paths], self.skip)
+        for k, v in groups.items():
+            basepaths = [Path(p) if isinstance(p, str) else p for p in v]
+            self.add_group(k, basepaths, self.skip)
+                
+    def add_group(self, name: str, basepaths: list[str | Path]) -> None:
+        if self.groups.get(name, None):
+            raise ValueError()
+        basepaths = [Path(p) if isinstance(p, str) else p for p in basepaths]
+        self.__basepaths.append(basepaths)
+        self.__groups[name] = Group(name, basepaths, self.skip)
+        
+    def remove_group(self, name: str):
+        group = self.__groups.pop(name)
+        [self.__basepaths.pop(i) for i in range(len(group.basepaths)) if self.basepaths[i] in group.basepaths]
+        
+    def get(self, group:str):
+        grp = self.groups.get(group, None)
+        if grp is None:
+            raise KeyError()
+        return grp
+    
+    def get_module(self, path: str | Path) -> PyModule | YamlModule:
+        if isinstance(path, str):
+            path = Path(path)
+        
+        module, groups = None, deque(self.groups)
+        while (module is None or len(groups) > 0):
+            group: Group = groups.popleft()
+            module = group.modules.get(path, None)
+        
+        if module is None:
+            raise KeyError()
+        return module
+            
+
+    def visit(self, *groups: Optional[str]) -> None:
+        grps = self._select_groups(groups)
+        [g.visit() for g in grps]
+        
+    def reload(self, *groups: Optional[str]) -> None:
+        grps = self._select_groups(groups)
+        [g.reload() for g in grps]
+        
+    def temporal_objects(self, *groups: Optional[str]) -> dict[str, Callable]:
+        grps = self._select_groups(groups)
+        return dict(ChainMap([g.temporal_objects() for g in grps]))
+    
+    def payloads(self, *groups: Optional[str]) -> dict[str, dict]:
+        grps = self._select_groups(groups)
+        return dict(ChainMap([g.payloads() for g in grps]))
+    
+    def inject(self, objects: dict[str, Callable], module_path: str | Path) -> None:
+        module = self.get_module(module_path)
+        if not isinstance(module, PyModule):
+            raise TypeError()
+        module.inject(objects)
+        
+    
+    def _select_groups(self, group_names: tuple[str]) -> list[Group]:
+        groups = self.groups.values()
+        if len(group_names) == 0:
+            groups = [self.get(g) for g in group_names]
+        return groups
+    
