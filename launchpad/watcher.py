9fac32bfb10741633f32e677a58bfcaeaa35c28c
morague <rom88.viry@gmail.com>
2024-08-06 15:55:22 +0200

    refactor(launchpad.watcher): typing

--- launchpad/watcher.py
+++ launchpad/watcher.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-
+from collections.abc import Sequence, Mapping
 import os
 import sys
 import glob
@@ -36,6 +36,7 @@ from launchpad.utils import (
 
 Datetime = str
 Payload = dict[str, Any]
+StrOrPath = str | Path
 
 logger = logging.getLogger("watcher")
 
@@ -76,23 +77,23 @@ class Module:
     changes: bool = False
 
     @property
-    def latest(self):
+    def latest(self) -> str:
         return self.__latest
 
     @property
-    def historics(self):
+    def historics(self) -> list[tuple[Datetime, str]]:
         return self.__historics
 
     @property
-    def module(self):
+    def module(self) -> Path:
         return self.__module
 
     @property
-    def ftype(self):
+    def ftype(self) -> str:
         return self.module.suffix.strip(".")
 
     @property
-    def from_package(self):
+    def from_package(self) -> bool:
         return "site-packages" in [p.name for p in self.module.parents]
 
     def __init__(self, module_fp: str | Path, new: bool= False) -> None:
@@ -105,10 +106,10 @@ class Module:
     def __repr__(self) -> str:
         return f"<{self.module}:latest {self.latest} | changes: {self.changes}>"
 
-    def version(self):
+    def version(self) -> str:
         return sha256(self.module.read_text().encode()).hexdigest()
 
-    def watch(self):
+    def watch(self) -> bool:
         changes = False
         version = self.version()
         if version != self.latest:
@@ -120,9 +121,9 @@ class Module:
     def changes_resolved(self):
         self.changes = False
 
-    def as_json(self) -> dict[str, Any]:
+    def as_json(self) -> Mapping[str, Any]:
         return {
-            "path": str(self.module.relative_to("./")),
+            "path": str(self.module),
             "latest": self.latest,
             "changes": self.changes,
             "historics": self.historics
@@ -135,7 +136,7 @@ class Module:
         self.__latest = version
         self.__historics.insert(0, ((datetime.now().isoformat(), self.latest)))
 
-    def _parse_path(self, path: str | Path):
+    def _parse_path(self, path: StrOrPath) -> Path:
         path = Path(path)
         if path.exists() is False:
             raise ValueError("path doesn't exist")
@@ -163,7 +164,7 @@ class PyModule(Module):
         super().__init__(module_fp, new)
 
     @property
-    def module_name(self):
+    def module_name(self) -> str:
         if self.from_package:
             rel_path = self._package_rel_path()
         else:
@@ -176,13 +177,16 @@ class PyModule(Module):
             self.module_name,
             self.module.absolute()
         )
+        if spec is None:
+            raise ValueError("specs from file not found.")
+
         module = importlib.util.module_from_spec(spec)
         sys.modules[self.module_name] = module
-        spec.loader.exec_module(module)
+        spec.loader.exec_module(module) # type: ignore
         self.changes_resolved()
         return module
 
-    def temporal_objects(self) -> dict[str, Type]:
+    def temporal_objects(self) -> Mapping[str, Type]:
         objects = {}
         for k,v in sys.modules[self.module_name].__dict__.items():
             if k.startswith("__"):
@@ -191,29 +195,29 @@ class PyModule(Module):
                 objects[k] = v
         return objects
 
-    def inject(self, objects: dict[str, Callable]) -> None:
+    def inject(self, objects: Mapping[str, Type]) -> None:
         sys.modules[self.module_name].__dict__.update(objects)
 
 class Group(object):
     __name: str
     __basepaths: list[Path]
-    __modules: dict[Path, Module]
+    __modules: dict[Path, PyModule | YamlModule]
     skips: list[Path]
 
     @property
-    def name(self):
+    def name(self) -> str:
         return self.__name
 
     @property
-    def modules(self):
+    def modules(self) -> Mapping[Path, PyModule | YamlModule]:
         return self.__modules
 
     @property
-    def basepaths(self):
+    def basepaths(self) -> Sequence[Path]:
         return self.__basepaths
 
     @property
-    def paths(self):
+    def paths(self) -> Sequence[Path]:
         paths = []
         for p in self.basepaths:
             if p in self.skips:
@@ -224,29 +228,29 @@ class Group(object):
                 paths.append(p)
         return paths
 
-    def __init__(self, name: str, paths: list[str | Path], skips: list[str | Path] = []) -> None:
+    def __init__(self, name: str, paths: Sequence[StrOrPath], skips: Sequence[StrOrPath] = []) -> None:
         self.__name = name
         self.__basepaths = to_path(paths)
         self.__modules = {}
         self.skips = to_path(skips)
         self.visit()
 
-    def add_paths(self, *paths) -> list[Path]:
-        paths = to_path(paths)
+    def add_paths(self, *paths: StrOrPath) -> None:
+        parsed_paths = to_path(paths)
         skipped = [p for p in self.skips if p in paths]
-        self.__basepaths = list(set(self.basepaths).union(set(paths)))
+        self.__basepaths = list(set(self.basepaths).union(set(parsed_paths)))
         self.skips = list(set(self.skips) - set(skipped))
         self.visit()
 
-    def remove_paths(self, *paths) -> list[Path]:
-        paths = to_path(paths)
-        skipping = [p for p in paths if p not in self.basepaths]
-        self.__basepaths = list(set(self.basepaths) - set(paths))
+    def remove_paths(self, *paths: StrOrPath) -> None:
+        parsed_paths = to_path(paths)
+        skipping = [p for p in parsed_paths if p not in self.basepaths]
+        self.__basepaths = list(set(self.basepaths) - set(parsed_paths))
         self.skips = list(set(self.skips).union(set(skipping)))
         self.visit()
 
     @log_group_visit
-    def visit(self):
+    def visit(self) -> Mapping[str, Sequence[Path]]:
         new = True
         if len(self.modules.keys()) == 0:
             new = False
@@ -256,33 +260,33 @@ class Group(object):
         return {"added": added, "modified": modified, "removed": removed}
 
     def load(self) -> None:
-        return [module.reload() for _, module in self.pymodules().items()]
+        [module.reload() for _, module in self.pymodules().items()]
 
     def reload(self) -> None:
-        return [module.reload() for _, module in self.pymodules().items() if module.changes]
+        [module.reload() for _, module in self.pymodules().items() if module.changes]
 
-    def inject(self, objects: dict[str, Callable]) -> None:
-        return [module.inject(objects) for _, module in self.pymodules().items()]
+    def inject(self, objects: Mapping[str, Type]) -> None:
+        [module.inject(objects) for _, module in self.pymodules().items()]
 
-    def temporal_objects(self) -> dict[str, Type]:
+    def temporal_objects(self) -> Mapping[str, Type]:
         objects = {}
         for _, module in self.pymodules().items():
             objects.update(module.temporal_objects())
         return objects
 
-    def payloads(self) -> dict[str, dict]:
+    def payloads(self) -> Mapping[str, Mapping]:
         payloads = {}
         for name, yaml in self.yamlmodules().items():
             payloads.update({name: yaml.load()})
         return payloads
 
     def pymodules(self) -> dict[str, PyModule]:
-        return {k:v for k,v in self.modules.items() if k.name.endswith(".py")}
+        return {k:v for k,v in self.modules.items() if isinstance(v, PyModule)} # type: ignore
 
     def yamlmodules(self) -> dict[str, YamlModule]:
-        return {k:v for k,v in self.modules.items() if (k.name.endswith(".yaml") or k.name.endswith(".yml"))}
+        return {k:v for k,v in self.modules.items() if isinstance(v, YamlModule)} # type: ignore
 
-    def _watch_modules(self):
+    def _watch_modules(self) -> Sequence[Path]:
         modified = []
         for path, module in self.modules.items():
             changes = module.watch()
@@ -290,7 +294,7 @@ class Group(object):
                 modified.append(path)
         return modified
 
-    def _register_module(self, new: bool= False):
+    def _register_module(self, new: bool= False) -> Sequence[Path]:
         registered = []
         new_paths = list(set(self.paths) - set(self.modules.keys()))
         for path in new_paths:
@@ -301,12 +305,12 @@ class Group(object):
                 self.__modules[path] = PyModule(path, new)
         return registered
 
-    def _remove_stale_module(self):
+    def _remove_stale_module(self) -> Sequence[Path]:
         removed_paths = list(set(self.modules.keys()) - set(self.paths))
         [self.__modules.pop(p) for p in removed_paths]
         return removed_paths
 
-    def _explore(self, path: Path) -> list[Path]:
+    def _explore(self, path: Path) -> Sequence[Path]:
         py = glob.glob(str(path.joinpath("**", "[!_]*.py")), recursive=True)
         yaml = glob.glob(str(path.joinpath("**", "[!_]*.yaml")), recursive=True)
         yml = glob.glob(str(path.joinpath("**", "[!_]*.yml")), recursive=True)
@@ -319,39 +323,39 @@ class Watcher(object):
     __groups: dict[str, Group]
 
     @property
-    def groups(self):
+    def groups(self) -> dict[str, Group]:
         return self.__groups
 
     @property
-    def basepaths(self):
+    def basepaths(self) -> Sequence[Path]:
         return list(chain.from_iterable([g.basepaths for g in self.groups.values()]))
 
     @property
-    def paths(self):
+    def paths(self) -> Sequence[Path]:
         return list(chain.from_iterable([g.paths for g in self.groups.values()]))
 
     @property
-    def skips(self):
+    def skips(self) -> Sequence[Path]:
         return list(chain.from_iterable([g.skips for g in self.groups.values()]))
 
     @property
-    def modules(self):
+    def modules(self) -> dict[str, Sequence[ PyModule | YamlModule]]:
         return {k:[m for m in v.modules.values()] for k,v in self.groups.items()}
 
     @property
-    def changed_modules(self):
+    def changed_modules(self) -> dict[str, Sequence[ PyModule | YamlModule]]:
         return {k:[m for m in v.modules.values() if m.changes] for k,v in self.groups.items()}
 
     @property
-    def unchanged_modules(self):
+    def unchanged_modules(self) -> dict[str, Sequence[ PyModule | YamlModule]]:
         return {k:[m for m in v.modules.values() if m.changes is False] for k,v in self.groups.items()}
 
-    def __init__(self, *paths: str | Path, **groups: Group) -> None:
+    def __init__(self, *paths: StrOrPath, **groups: Group) -> None:
         self.__groups = {k:v for k,v in groups.items()}
         if paths:
             self.add_group("others", paths, [])
 
-    def add_group(self, name: str, basepaths: list[str | Path], skips: list[str | Path]) -> None:
+    def add_group(self, name: str, basepaths: Sequence[StrOrPath], skips: Sequence[StrOrPath]) -> None:
         if self.groups.get(name, None):
             raise ValueError()
         basepaths = to_path(basepaths)
@@ -360,25 +364,25 @@ class Watcher(object):
     def remove_group(self, name: str):
         group = self.__groups.pop(name)
 
-    def add_paths(self, group_name: str, paths: list[str | Path]) -> None:
+    def add_paths(self, group_name: str, paths: list[StrOrPath]) -> None:
         group = self.groups.get(group_name, None)
         if group is None:
             raise ValueError("group does not exist")
         group.add_paths(*paths)
 
-    def remove_paths(self, group_name: str, paths: list[str | Path]) -> None:
+    def remove_paths(self, group_name: str, paths: list[StrOrPath]) -> None:
         group = self.groups.get(group_name, None)
         if group is None:
             raise ValueError("group does not exist")
         group.remove_paths(*paths)
 
-    def get(self, group:str):
+    def get(self, group: str) -> Group:
         grp = self.groups.get(group, None)
         if grp is None:
             raise KeyError("group does not exist")
         return grp
 
-    def get_module(self, path: str | Path, default: Any= None) -> PyModule | YamlModule:
+    def get_module(self, path: StrOrPath, default: Any= None) -> PyModule | YamlModule:
         module, groups = None, deque(self.groups.values())
         while (module is None and len(groups) > 0):
             group: Group = groups.popleft()
@@ -386,7 +390,7 @@ class Watcher(object):
         return module or default
 
     @log_watcher_visit
-    def visit(self, *groups: Optional[str]) -> None:
+    def visit(self, *groups: str) -> Mapping[str, Any]:
         res = {}
         grps = self._select_groups(groups)
         for group in grps:
@@ -394,58 +398,64 @@ class Watcher(object):
             res[group.name] = changes
         return res
 
-    def reload(self, *groups: Optional[str]) -> None:
+    def reload(self, *groups: str) -> None:
         grps = self._select_groups(groups)
         [g.reload() for g in grps]
 
-    def load(self, *groups: Optional[str]) -> None:
+    def load(self, *groups: str) -> None:
         grps = self._select_groups(groups)
         [g.load() for g in grps]
 
-    def inject(self, *groups: Optional[str], objects: dict[str, Callable]) -> None:
+    def inject(self, *groups: str, objects: Mapping[str, Type]) -> None:
         grps = self._select_groups(groups)
         [g.inject(objects) for g in grps]
 
-    def temporal_objects(self, *groups: Optional[str]) -> dict[str, Type]:
+    def temporal_objects(self, *groups: str) -> Mapping[str, Type]:
         grps = self._select_groups(groups)
         objects = {}
         [objects.update(g.temporal_objects()) for g in grps]
         return objects
 
-    def payloads(self, *groups: Optional[str]) -> dict[str, dict]:
+    def payloads(self, *groups: str) -> Mapping[str, Mapping]:
         grps = self._select_groups(groups)
-        return dict(ChainMap([g.payloads() for g in grps]))
+        return dict(ChainMap({g.name:g.payloads() for g in grps}))
 
-    def reload_module(self, module_path: str | Path) -> None:
+    def reload_module(self, module_path: StrOrPath) -> None:
         module = self.get_module(module_path)
+        if isinstance(module, YamlModule):
+            raise ValueError("YamlModule has no method temporal_objects")
         module.reload()
 
-    def temporal_objects_from_module(self, module_path: str | Path) -> dict[str, Type]:
+    def temporal_objects_from_module(self, module_path: StrOrPath) -> Mapping[str, Type]:
         module = self.get_module(module_path)
+        if isinstance(module, YamlModule):
+            raise ValueError("YamlModule has no method temporal_objects")
         return module.temporal_objects()
 
-    def payload_from_module(self, module_path: str | Path) -> dict[str, dict]:
+    def payload_from_module(self, module_path: StrOrPath) -> Mapping[str, Mapping]:
         module = self.get_module(module_path)
+        if isinstance(module, PyModule):
+            raise ValueError("Pymodule has no method payload")
         return module.payload()
 
-    def inject_module(self, objects: dict[str, Callable], module_path: str | Path) -> None:
+    def inject_module(self, objects: Mapping[str, Type], module_path: StrOrPath) -> None:
         module = self.get_module(module_path)
         if not isinstance(module, PyModule):
             raise TypeError()
         module.inject(objects)
 
-    def _select_groups(self, group_names: tuple[str]) -> list[Group]:
+    def _select_groups(self, group_names: Sequence[str]) -> Sequence[Group]:
         groups = self.groups.values()
         if len(group_names) > 0:
             groups = [self.get(g) for g in group_names]
-        return groups
+        return groups # type: ignore
 
 
 class LaunchpadWatcher(Watcher):
     watcher: Process | None = None
     polling_interval: int = 600
     automatic_refresh: bool = True
-    base_modules: dict[str, list[str | Path | Traversable]] = {
+    base_modules: dict[str, list[StrOrPath | Traversable]] = {
         "workflows": [files("launchpad").joinpath("workflows.py")],
         "workers": [files("launchpad").joinpath("workers.py")],
         "runners": [files("launchpad").joinpath("runners.py")],
@@ -453,40 +463,40 @@ class LaunchpadWatcher(Watcher):
         "temporal": [files("launchpad").joinpath("temporal_server.py")]
     }
 
-    def __init__(self, *paths: str | Path,  **groups: Group) -> None:
+    def __init__(self, *paths: StrOrPath,  **groups: Group) -> None:
         super().__init__(*paths, **groups)
 
     @classmethod
-    def initialize(cls, *paths,**groups: dict[str, list[str | Path]]) -> LaunchpadWatcher:
+    def initialize(cls, *paths,**groups: dict[str, list[StrOrPath]]) -> LaunchpadWatcher:
         basegroups = {}
         for k in list(set(cls.base_modules).union(set(groups.keys()))):
             basepaths = cls.base_modules.get(k, [])
             group = groups.get(k, {})
             skips = group.get("skips", [])
             basepaths.extend(group.get("basepaths", []))
-            basegroups.update({k:Group(k, basepaths, skips)})
+            basegroups.update({k:Group(k, basepaths, skips)}) # type: ignore
         return cls(*paths, **basegroups)
 
 
-    def workers_settings(self) -> dict[str, dict[str, Any]]:
+    def workers_settings(self) -> Mapping[str, Mapping[str, Any]]:
         return {v["worker"]["task_queue"]:v for v in self.get("workers").payloads().values()}
 
-    def tasks_settings(self) -> dict[str, dict[str, Any]]:
+    def tasks_settings(self) -> Mapping[str, Mapping[str, Any]]:
         return {v["name"]:v for v in self.get("deployments").payloads().values()}
 
-    def activities(self) -> dict[str, Type]:
+    def activities(self) -> Mapping[str, Type]:
         return {k:v for k,v in self.get("activities").temporal_objects().items() if is_activity(v)}
 
-    def workflows(self) -> dict[str, Type]:
+    def workflows(self) -> Mapping[str, Type]:
         return {k:v for k,v in self.get("workflows").temporal_objects().items() if is_workflow(v)}
 
-    def workers(self) -> dict[str, Type]:
+    def workers(self) -> Mapping[str, Type]:
         return {k:v for k,v in self.get("workers").temporal_objects().items() if is_temporal_worker(v)}
 
-    def runners(self) -> dict[str, Type]:
+    def runners(self) -> Mapping[str, Type]:
         return {k:v for k,v in self.get("runners").temporal_objects().items() if is_runner(v)}
 
-    def configs(self):
+    def configs(self) -> Sequence[Mapping[str, Any]]:
         return [v for v in self.get("configs").payloads().values()]
 
     async def poll(self, app: Sanic) -> None:
